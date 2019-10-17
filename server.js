0942b9acf9c096db67226c6bdd47c2c1ba0e2c91
Saurabh Shukla <saurabh.shukla244@gmail.com>
2019-10-17 20:14:55 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to mongoose.'));
+
+app.use('/', indexRouter);
+
+
+app.listen( port, () => {
+    console.log(`Server started on port ${port}`);
+});
